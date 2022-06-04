02d2e8f5ba5f1e59f8bc71379123d72d75f2de49
Bucket <jduffmai@uwaterloo.ca>
2022-06-03 22:35:01 -0400

    Pausing rest development in favour of GraphQL

---
+++ StratzDotNet/Models/GraphQL/Data.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StratzDotNet.Models
+{
+    public class Data<T>
+    {
+        public T Result { get; set; }
+    }
+}
